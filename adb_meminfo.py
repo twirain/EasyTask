b0cc419bff9ada2b46e71ff47655d9a884dc443e
zhanbaoquan <zhanbaoquan@cmcm.com>
2023-05-29 20:10:13 +0800

    add: meminfo dump util

---
+++ adb_meminfo.py
@@ -0,0 +1,32 @@
+import argparse
+import errno
+import os
+
+import output
+import utils
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='util to dumpsys memory info by meminfo command.')
+    parser.add_argument('packageName')
+    parser.add_argument('-o', '--output', default='dump_meminfo.txt')
+
+    args = parser.parse_args()
+    package_name = args.packageName
+    package = utils.get_package(package_name)
+
+    if package is None:
+        output.print_red('应用不存在')
+        exit(errno.EINVAL)
+
+    dev = utils.get_devices()
+    if dev is None:
+        output.print_red('没有检测到设备')
+        exit(errno.ENODEV)
+
+    pid = utils.get_pid(package_name)
+    if pid is None:
+        output.print_red('该进程未在运行状态')
+        exit(errno.ESRCH)
+    dump_ret = os.system(f'adb -s {dev} shell dumpsys meminfo {pid} > {args.output}')
+    if dump_ret == 0:
+        print(f'文件已保存到：{os.path.abspath(args.output)}')
