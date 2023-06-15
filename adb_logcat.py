4d0253aee5be9f8ae677fd7ff85d673f22f853f6
zhanbaoquan <zhanbaoquan@cmcm.com>
2023-06-15 10:49:15 +0800

    feat: logcat support filterspeces feature

--- adb_logcat.py
+++ adb_logcat.py
@@ -1,13 +1,30 @@
+import argparse
+import io
 import os
+import re
 
-import output
 import utils
 
 if __name__ == '__main__':
-    if len(os.sys.argv) < 2:
-        output.print_red('未指定用于过滤的部分进程名')
-        exit(1)
-    part_name = str(os.sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help='part of process name to easy match')
+    parser.add_argument('-f', '--filter-specs', nargs='+', required=False,
+                        help='format is <tag>/<level>, use to filter log. detail see logcat help')
+    args = parser.parse_args()
+    specs = args.filter_specs
+
+    filter_specs = io.StringIO()
+    for spec in specs:
+        if re.match(r'(.+):(V|D|I|W|E|F)', spec):
+            filter_specs.write(spec)
+            filter_specs.write(' ')
+
     dev = utils.get_devices()
-    pid = utils.get_pid(part_name, dev)
-    os.system(f'adb -s {dev} shell logcat --pid={pid}')
+    pid = utils.get_pid(args.name, dev)
+
+    if specs is None:
+        os.system(f'adb -s {dev} shell logcat --pid={pid}')
+    else:
+        # https://developer.android.google.cn/studio/command-line/logcat?hl=zh-cn#filteringOutput
+        # 比如：“OCPA:V *:S”表示全部日志静默、OCPA输出V以上等级的日志
+        os.system(f'adb -s {dev} shell logcat --pid={pid} "{filter_specs.getvalue()} *:S"')
